2193e83ecc5c4caf80d0e543ac755b5e6060b546
Tray <andreytrayss@gmail.com>
2025-02-14 22:11:38 +0300

    ChangeDetector
    Добавлена проверка на вора

--- Assets/Scripsts/Detector.cs
+++ Assets/Scripsts/Detector.cs
@@ -6,7 +6,15 @@ public class Detector : MonoBehaviour
     public event Action RogueHasLeft;
     public event Action RogueCome;
 
-    private void OnTriggerEnter(Collider other) => RogueCome?.Invoke();
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Rogue>(out _))
+            RogueCome?.Invoke();
+    }
 
-    private void OnTriggerExit(Collider other) => RogueHasLeft?.Invoke();
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<Rogue>(out _))
+            RogueHasLeft?.Invoke();
+    }
 }
